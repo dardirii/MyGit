9e49680b6f94226081166223bd12b5d3a915948e
unknown <dardirii>
2023-04-03 15:10:13 +0700

    modif data

--- src/index.js
+++ src/index.js
@@ -41,10 +41,11 @@ class Table {
   }
   
   const table = new Table({
-    columns: ["Name", "Email"],
+    columns: ["Tittle", "Description"],
     data: [
-      ["Edi Hartono", "edi.eduwork@gmail.com"],
-      ["Dodi Prakoso", "dodi@upscale.id"]
+      ["iPhone 9", "An apple mobile which is nothing like apple"],
+      ["iPhone X", "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip"]
+      ["Huawei P30", "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK"]
     ]
   });
   const app = document.getElementById("app");
